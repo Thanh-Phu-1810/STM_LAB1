6091a8cf6f45b1f013ecfdb953e07a4dcc13e68f
LAPTOP-3Q74QG63\Thanh Phu <phu.lelethanhphu@hcmut.edu.vn>
2024-09-24 00:35:17 +0700

    Init for Ex6

--- LAB1/Debug/Core/Src/exercise6.d
+++ LAB1/Debug/Core/Src/exercise6.d
@@ -1,4 +1,4 @@
-Core/Inc/exercise6.o: ../Core/Inc/exercise6.c ../Core/Inc/exercise6.h \
+Core/Src/exercise6.o: ../Core/Src/exercise6.c ../Core/Inc/exercise6.h \
  ../Core/Inc/main.h ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
