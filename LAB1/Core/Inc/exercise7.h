40f9f9bb8b7da43c675448cb5d8946909f56330f
LAPTOP-3Q74QG63\Thanh Phu <phu.lelethanhphu@hcmut.edu.vn>
2024-09-24 00:48:11 +0700

    Init for Ex7

---
+++ LAB1/Core/Inc/exercise7.h
@@ -0,0 +1,16 @@
+/*
+ * exercise7.h
+ *
+ *  Created on: Sep 10, 2024
+ *      Author: Thanh Phu
+ */
+
+#ifndef INC_EXERCISE7_H_
+#define INC_EXERCISE7_H_
+#include "main.h"
+void EXERCISE7_Run();
+void clearAllClock();
+
+
+
+#endif /* INC_EXERCISE7_H_ */
